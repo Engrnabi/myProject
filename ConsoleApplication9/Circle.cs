91e8ffe6fa6cc140d4a3e766f517b5fde6382fc1
Engrnabi <engrnabi@gmail.com>
2015-03-06 14:35:43 +0600

    Initial Commit

---
+++ ConsoleApplication9/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Circle
+    {
+        public double Radious { set; get; }
+        public double Diameter { set; get; }
+        public double Perimeter { set; get; }
+        public double  Area { set; get; }
+
+
+        public double circleArea()
+        {
+            return Math.PI*Radious*Radious;
+        }
+
+         public double circlePerimeter()
+        {
+            return Math.PI*Radious*2;
+        }
+         public double circleDiameter()
+        {
+            return 2*Radious;
+        }
+    }
+}
---
+++ ConsoleApplication9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            Console.WriteLine("Enter the Value of Radious");
+            aCircle.Radious = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area=" + aCircle.circleArea() + "\n Perimeter=" + aCircle.circlePerimeter() +
+                              "\n Diameter=" + aCircle.circleDiameter());
+            Console.ReadKey();
+
+        }
+        
+
+    }
+}
